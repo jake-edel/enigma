8d6258ac69d0f883bac3d15e046ab8ffc002f20b
Jakob Edelstein <jakobedelstein@gmail.com>
2022-01-13 13:14:07 -0700

    test: set up tests for Enigma class

---
+++ lib/enigma.rb
@@ -0,0 +1,9 @@
+class Enigma
+  def self.encrypt(encryption_hash)
+    encryption_hash
+  end
+
+  def self.decrypt(encryption_hash)
+    encryption_hash
+  end
+end
---
+++ spec/enigma_spec.rb
@@ -0,0 +1,24 @@
+require './lib/enigma'
+
+RSpec.describe Enigma do
+  before(:each) do
+    @plain_text = {
+      message: 'Hello World',
+      key: '02715',
+      date: '040895'
+    }
+
+    @encrypted = Enigma.encrypt(@plain_text)
+    @decrypted = Enigma.decrypt(@encrypted)
+  end
+
+  it 'takes a hash with a string, a key, and a date, encrypts message, and returns a hash with an encryped string, key, and date' do
+    expect(@encrypted).to be_instance_of Hash
+  end
+
+  it 'takes a hash with an encrypted cipher text, an decryption key, and decryption date, and returns a hash with a decrypted message, key and date' do
+    require 'pry-byebug'; binding.pry
+    expect(@decrypted).to be_instance_of Hash
+  end
+
+ end
