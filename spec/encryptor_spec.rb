ec73ce4d3baff4b95d7a3abf20e0be69c0bffe5b
Jakob Edelstein <jakobedelstein@gmail.com>
2022-01-13 14:23:22 -0700

    test: flesh out test

--- spec/encryptor_spec.rb
+++ spec/encryptor_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Encryptor do
 
     it 'takes a string as arg and returns an array of consecutive numbers stored in arrays' do
       consecutive_values = Encryptor.find_consecutive_values(@key)
+      expect(consecutive_values.all? { |cons| cons.to_i.instance_of?(Integer) }).to be true
       expect(consecutive_values).to eq [ '12', '23', '34', '45' ]
     end
   end
