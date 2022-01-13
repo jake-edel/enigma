e6d87e56118627ad4aabf4c65904ea65d831d8fa
Jakob Edelstein <jakobedelstein@gmail.com>
2022-01-13 15:19:19 -0700

    feat: offsets_from_date returns 4 offset values

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,11 +1,15 @@
 class Encryptor
 
   def self.find_shifts(key, date)
-    consec_keys = find_consecutive_values(key)
-
+    keys = find_consecutive_values(key)
+    offsets = offsets_from_date(date)
   end
 
   def self.find_consecutive_values(string)
     string.chars.each_cons(2).map { |cons| cons.join('').to_i }
+  end
+
+  def self.offsets_from_date(string)
+    (string.to_i ** 2).to_s[-4..-1].split('').map(&:to_i)
   end
 end
