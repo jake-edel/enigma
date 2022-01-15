772edb4d30b85c64370a5db8778e7c5345b15aea
Jakob Edelstein <jakobedelstein@gmail.com>
2022-01-15 11:42:42 -0700

    encrypt.rb takes 2 file paths as cl args, encrypts first file, and writes encryption to second

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,7 +1,34 @@
 require './lib/enigma'
 
-require 'pry-byebug'; binding.pry
+class Interface < Enigma
+  def initialize
+    @message_file_path = './msgs/' + ARGV.first
+    @encryption_file_path = './msgs/' + ARGV[1]
+    read_message
+    encrypt_message
+    write_encrypted_message
+    print_output
+ 	end
 
+  def read_message
+    File.open(@message_file_path, 'rb') do |file|
+      @plain_text = file.read.chomp
+    end
+  end
 
+  def encrypt_message
+    @encryption_hash = encrypt(@plain_text)
+  end
 
-puts
+  def write_encrypted_message
+    File.open(@encryption_file_path, 'wb') do |file|
+      file << @encryption_hash
+    end
+  end
+
+  def print_output
+    puts "Created '#{ARGV.first}' with the key #{@encryption_hash[:key]} and the date #{@encryption_hash[:date]}"
+  end
+end
+
+Interface.new
