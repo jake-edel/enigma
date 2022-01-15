454d9bb6f4a68ae6cdb69da6d9927eaba09a1fc9
Jakob Edelstein <jakobedelstein@gmail.com>
2022-01-15 08:40:27 -0700

    feat: generate_date will return a 6 digit string generated from the current date

--- lib/key_gen.rb
+++ lib/key_gen.rb
@@ -1,12 +1,16 @@
 class KeyGen
 
- 	def self.generate_key
+  def self.generate_key
     key = ''
     5.times { key += rand(9).to_s }
     key
   end
 
   def self.generate_date
-    require 'pry-byebug'; binding.pry
+    today = Time.now
+    year = today.year.to_s[-2..-1]
+    month = today.month.to_s.rjust(2, '0')
+    day = today.day.to_s.rjust(2, '0')
+    year + month + day
   end
 end
